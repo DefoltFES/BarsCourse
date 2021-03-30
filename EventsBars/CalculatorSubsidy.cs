feb1567711dd41e108249a5f80f1593832752e47
DefoltFES <mironoff.emil@yandex.ru>
2021-03-30 21:22:24 +0300

    Add project files.

---
+++ EventsBars/CalculatorSubsidy.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsBars
+{
+    class CalculatorSubsidy : ISubsidyCalculation
+    {
+        public event EventHandler<string> OnNotify;
+        public event EventHandler<Tuple<string, Exception>> OnException;
+
+        public Charge CalculateSubsidy(Volume volumes, Tariff tariff)
+        {
+            OnNotify(this, $"Расчет начат {DateTime.Today.ToShortTimeString()}");
+            Charge charge = new Charge { ServiceId= volumes.HouseId,HouseId=volumes.HouseId,Month=volumes.Month,Value=volumes.Value*tariff.Value };
+            OnNotify(this, $"Расчет закончен {DateTime.Today.ToShortTimeString()}");
+            return charge;
+        }
+    }
+}
