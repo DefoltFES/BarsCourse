dd249be2c8996415e4b21f457bb929cf94b8ab4a
DefoltFES <mironoff.emil@yandex.ru>
2021-04-11 17:17:11 +0300

    Add Redis

---
+++ Chat1/Chat1/Program.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+
+namespace Chat1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var redis = ConnectionMultiplexer.Connect("localhost");
+            var db = redis.GetDatabase(0);
+            while (Console.ReadLine() != "end")
+            {
+                int x1 = Convert.ToInt32(Console.ReadLine());
+                int x2 = Convert.ToInt32(Console.ReadLine());
+                var list = new RedisValue[]{ x1, x2 };
+                db.ListRightPush("numbers", list);
+
+            }
+        }
+    }
+}
---
+++ Chat2/Chat2/Program.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Chat2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var redis = ConnectionMultiplexer.Connect("localhost");
+            var db = redis.GetDatabase(0);
+            while (Console.ReadLine() != "end")
+            {
+                int x1 = (int)db.ListRightPop("numbers");
+                Console.WriteLine("Число 1 "+x1);
+                int x2 = (int)db.ListRightPop("numbers");
+                Console.WriteLine("Число 2 " + x2);
+                Console.WriteLine($"Результат {x1+x2}");
+                
+
+            }
+        }
+    }
+}
